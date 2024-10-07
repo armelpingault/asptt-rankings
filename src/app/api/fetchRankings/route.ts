10609eb82d755b2b80ef8a20e2e0f49da7669fc4
Armel <armel.pingault@gmail.com>
2024-10-07 21:43:51 +0200

    Update route.ts

--- src/app/api/fetchRankings/route.ts
+++ src/app/api/fetchRankings/route.ts
@@ -19,7 +19,7 @@ async function fetchRankings(url: string) {
     defaultViewport: chromium.defaultViewport,
     executablePath: await chromium.executablePath(
       // "https://github.com/Sparticuz/chromium/releases/download/v129.0.0/chromium-v129.0.0-pack.tar"
-      "https://chromium.armelpingault.com/chromium-v119.0.0-pack.tar"
+      "https://chromium.armelpingault.com/chromium-v129.0.0-pack.tar"
     ),
     headless: chromium.headless,
     ignoreHTTPSErrors: true,
